9ebe0bdf76039fcbca384a1965ad743829cd6056
Rob Knop (Nersc) <raknop@lbl.gov>
2022-05-06 13:33:30 -0700

    removing some things from settings that don't have a corresponding thing in the .ini file; change database to postgres

--- YSE_PZ/settings.py
+++ YSE_PZ/settings.py
@@ -49,44 +49,44 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'widget_tweaks',
-	'django_tables2',
+    'django_tables2',
     'explorer',
-	'bootstrap3',
-	'django_filters',
-	'django_cron',
-	'el_pagination',
+    'bootstrap3',
+    'django_filters',
+    #'django_cron',
+    'el_pagination',
     'auditlog',
-	#'silk'
+    #'silk'
 ]
 
-CRON_CLASSES = [
-	'YSE_App.data_ingest.TNS_uploads.TNS_emails',
-	'YSE_App.data_ingest.Photo_Z.YSE',
-	'YSE_App.data_ingest.SDSS_Photo_Z.YSE',
-	'YSE_App.data_ingest.PS1_cutouts.YSE',
-	'YSE_App.data_ingest.host_associate.YSE',
-	'YSE_App.data_ingest.PS1_PhotoZ.YSE',
-	'YSE_App.data_ingest.Apply_Tags.Tags',
-	'YSE_App.data_ingest.YSE_observations.SurveyObs',
-	'YSE_App.data_ingest.Query_ZTF.AntaresZTF',
-	'YSE_App.data_ingest.QUB_data.YSE',
-	'YSE_App.data_ingest.Gaia_LC.GaiaLC',
-	'YSE_App.data_ingest.QUB_data.YSE_Weekly',
-	'YSE_App.data_ingest.QUB_data.QUB',
-	'YSE_App.data_ingest.QUB_data.YSE_Stack',
-    'YSE_App.data_ingest.DECam_upload.DECam',
-    'YSE_App.rapid.rapid_classify.rapid_classify_cron',
-	'YSE_App.data_ingest.YSE_Forced_Phot.ForcedPhot',
-	'YSE_App.data_ingest.YSE_Forced_Phot.ForcedPhotUpdate',
-	'YSE_App.data_ingest.TNS_uploads.TNS_updates',
-	'YSE_App.data_ingest.TNS_uploads.TNS_Ignore_updates',
-	'YSE_App.data_ingest.TNS_uploads.TNS_recent',
-	'YSE_App.data_ingest.TNS_uploads.TNS_recent_realtime',
-    'YSE_App.data_ingest.QUB_data.CheckDuplicates',
-    'YSE_App.data_ingest.PhotometryUploadExample.PhotometryUploads'
-    'YSE_App.data_ingest.ZTF_Forced_Phot_Cron.ForcedPhot',
-    'YSE_App.data_ingest.TNS_uploads.UpdateGHOST'
-]
+# CRON_CLASSES = [
+# 	'YSE_App.data_ingest.TNS_uploads.TNS_emails',
+# 	'YSE_App.data_ingest.Photo_Z.YSE',
+# 	'YSE_App.data_ingest.SDSS_Photo_Z.YSE',
+# 	'YSE_App.data_ingest.PS1_cutouts.YSE',
+# 	'YSE_App.data_ingest.host_associate.YSE',
+# 	'YSE_App.data_ingest.PS1_PhotoZ.YSE',
+# 	'YSE_App.data_ingest.Apply_Tags.Tags',
+# 	'YSE_App.data_ingest.YSE_observations.SurveyObs',
+# 	'YSE_App.data_ingest.Query_ZTF.AntaresZTF',
+# 	'YSE_App.data_ingest.QUB_data.YSE',
+# 	'YSE_App.data_ingest.Gaia_LC.GaiaLC',
+# 	'YSE_App.data_ingest.QUB_data.YSE_Weekly',
+# 	'YSE_App.data_ingest.QUB_data.QUB',
+# 	'YSE_App.data_ingest.QUB_data.YSE_Stack',
+#     'YSE_App.data_ingest.DECam_upload.DECam',
+#     'YSE_App.rapid.rapid_classify.rapid_classify_cron',
+# 	'YSE_App.data_ingest.YSE_Forced_Phot.ForcedPhot',
+# 	'YSE_App.data_ingest.YSE_Forced_Phot.ForcedPhotUpdate',
+# 	'YSE_App.data_ingest.TNS_uploads.TNS_updates',
+# 	'YSE_App.data_ingest.TNS_uploads.TNS_Ignore_updates',
+# 	'YSE_App.data_ingest.TNS_uploads.TNS_recent',
+# 	'YSE_App.data_ingest.TNS_uploads.TNS_recent_realtime',
+#     'YSE_App.data_ingest.QUB_data.CheckDuplicates',
+#     'YSE_App.data_ingest.PhotometryUploadExample.PhotometryUploads'
+#     'YSE_App.data_ingest.ZTF_Forced_Phot_Cron.ForcedPhot',
+#     'YSE_App.data_ingest.TNS_uploads.UpdateGHOST'
+# ]
 
 MIDDLEWARE = [
 	#'silk.middleware.SilkyMiddleware',
@@ -178,7 +178,7 @@ DATABASES = {
         'PASSWORD': POSTGRESQL_PASSWORD,
         'HOST': os.getenv( 'DB_HOST', 'yse-pz-rknop-postgres' ),
         'PORT': os.getenv( 'DB_PORT', '5432' )
-    }
+    },
     'explorer': {
         'ENGINE': os.getenv( 'DB_ENGINE', 'django.db.backends.postgresql' ),
         'NAME': os.getenv( 'DB_NAME', 'yse_pz' ),
@@ -233,16 +233,16 @@ SMTP_LOGIN = config.get('SMTP_provider', 'SMTP_LOGIN')
 SMTP_PASSWORD = config.get('SMTP_provider', 'SMTP_PASSWORD')
 SMTP_HOST = config.get('SMTP_provider', 'SMTP_HOST')
 SMTP_PORT = config.get('SMTP_provider', 'SMTP_PORT')
-LCOGTUSER = config.get('main', 'lcogtuser')
-LCOGTPASS = config.get('main', 'lcogtpass')
-ZTFPASS = config.get('yse_forcedphot','ztfforcedphotpass')
+# LCOGTUSER = config.get('main', 'lcogtuser')
+# LCOGTPASS = config.get('main', 'lcogtpass')
+# ZTFPASS = config.get('yse_forcedphot','ztfforcedphotpass')
 KEPLER_API_ENDPOINT = "http://api.keplerscience.org/is-k2-observing"
-TNSUSER = config.get('main','tns_bot_name')
-TNSID = config.get('main','tns_bot_id')
+# TNSUSER = config.get('main','tns_bot_name')
+# TNSID = config.get('main','tns_bot_id')
 TNSAPIKEY = config.get('main','tnsapikey')
-TNSDECAMUSER = config.get('main','tns_decam_bot_name')
-TNSDECAMID = config.get('main','tns_decam_bot_id')
-TNSDECAMAPIKEY = config.get('main','tnsdecamapikey')
+# TNSDECAMUSER = config.get('main','tns_decam_bot_name')
+# TNSDECAMID = config.get('main','tns_decam_bot_id')
+# TNSDECAMAPIKEY = config.get('main','tnsdecamapikey')
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
@@ -266,7 +266,7 @@ STATIC_URL = config.get('site_settings', 'STATIC')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
-ZTFTMPDIR = config.get('yse_forcedphot','ztfforcedtmpdir')
+# ZTFTMPDIR = config.get('yse_forcedphot','ztfforcedtmpdir')
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 USE_X_FORWARDED_HOST = True
